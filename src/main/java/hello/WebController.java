0467436d069d2a23e66861fddc441d92ef5863fb
Jingjing Yu <jingjingyu@ucsb.edu>
2019-11-20 21:45:11 -0800

    JJY-added service that will eventually get earthquake data

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -1,5 +1,5 @@
 package hello;
-
+import hello.geojson.FeatureCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -8,6 +8,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -15,6 +17,12 @@ import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 
 @Controller
 public class WebController {
+
+    @GetMapping("/earthquakes/search")
+    public String getEarthquakesSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            EqSearch eqSearch) {
+        return "earthquakes/search";
+    }
 
     @Autowired
     private ClientRegistrationRepository clientRegistrationRepository;
@@ -24,6 +32,19 @@ public class WebController {
         return "index";
     }
 
+    @GetMapping("/earthquakes/results")
+    public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            EqSearch eqSearch) {
+        EarthquakeQueryService e =
+            new EarthquakeQueryService();
+        model.addAttribute("eqSearch", eqSearch);
+        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
+        model.addAttribute("json", json);
+        FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
+        model.addAttribute("featureCollection",featureCollection);
+        return "earthquakes/results";
+    }
+    
     @GetMapping("/login")
     public String getLoginPage(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken) {
 
@@ -48,18 +69,5 @@ public class WebController {
     public String getPage2(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken) {
 
         return "page2";
-    }
-     @GetMapping("/earthquakes/search")
-    public String getEarthquakesSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
-            EqSearch eqSearch) {
-        return "earthquakes/search";
-    }
-
-     @GetMapping("/earthquakes/results")
-    public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
-            EqSearch eqSearch) {
-        model.addAttribute("eqSearch", eqSearch);
-        // TODO: Actually do the search here and add results to the model
-        return "earthquakes/results";
     }
 }
